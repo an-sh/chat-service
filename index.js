896189d25bd24167ea3fc1dd04e646670f30373b
an-sh <an-sh@users.noreply.github.com>
2016-08-12 16:35:08 +0300

    style: index.js

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
+'use strict'
+
 var semver = require('semver')
 
+// Choosing between a source ES6 syntax and a babel transpired
+// ES5. Note that ES6 source code uses only functions that are
+// available in ES5 environment (or lodash 4.x equivalents), so no
+// globals modifications are required.
 if (semver.lt(process.version, '6.0.0')) {
   module.exports = require('./lib/ChatService')
 } else {
