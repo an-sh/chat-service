040db82d804a7048eb26f2e6b9105125cf16e843
an-sh <an.sh.1@yandex.ru>
2019-12-14 20:06:53 +0300

    chore: update
    BREAKING CHANGE: Now only Node.js version 10 and later are supported

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
 'use strict'
 
-var semver = require('semver')
-
-// Choosing between a source ES6 syntax and a transpiled ES5.
-if (semver.lt(process.version, '6.0.0')) {
-  module.exports = require('./lib/ChatService')
-} else {
-  module.exports = require('./src/ChatService')
-}
+module.exports = require('./src/ChatService')
